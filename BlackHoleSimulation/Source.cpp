82f0c4a6bd05e9ef60914a75c0710f1a708edaa0
adnankuran0 <prodarzyn@gmail.com>
2024-12-14 06:05:05 +0300

    added gui

--- BlackHoleSimulation/Source.cpp
+++ BlackHoleSimulation/Source.cpp
@@ -1,4 +1,7 @@
 #include "GL/glew.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include <GLFW/glfw3.h>
 #include "Shader.h"
 #include <iostream>
@@ -17,6 +20,7 @@ const unsigned int SCR_HEIGHT = 720;
 int main(void)
 {
     GLFWwindow* window;
+
     
     glm::vec2 resolution(SCR_WIDTH, SCR_HEIGHT);
     glm::vec4 mouse(0.0f, 0.0f, 0.0f, 0.0f);
@@ -27,8 +31,22 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Black Hole Simulation", NULL, NULL);
+
+
+    int AA = 2;
+    float size = 0.3f;
+    bool showDisk = true;
+    float speed = 3.0f;
+    float steps = 12.0f;
+    bool fixedCamera = true;
+    float distance = 1024.0f;
+    float rotation = 355.0f;
+
+
+
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     if (!window)
     {
@@ -42,6 +60,12 @@ int main(void)
     {
         std::cout << "GLEW initialization ERROR!" << std::endl;
     }
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui::StyleColorsClassic();
+    ImGui_ImplOpenGL3_Init((char*)glGetString(330));
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
@@ -96,7 +120,7 @@ int main(void)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load and generate the texture
     int width, height, nrChannels;
-    unsigned char* data = stbi_load("test.jpg", &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load("nebula.png", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -124,20 +148,41 @@ int main(void)
         shader.setVec2("resolution", resolution);
         glBindTexture(GL_TEXTURE_2D, texture);
         glfwGetCursorPos(window, &mouseX, &mouseY);
-        mouse[0] = (float)mouseX;
-        mouse[1] = (float)mouseY;
+        mouse[0] = !fixedCamera ? (float)mouseX : distance;
+        mouse[1] = !fixedCamera ? (float)mouseY : rotation;
+
 
         mouse[2] = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) ? 1.0f : 0.0f;
         mouse[3] = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) ? 1.0f : 0.0f;
         shader.setVec4("mouse", mouse);
-        shader.setFloat("speed", 3.0f);
-        shader.setInt("AA", 2);
-        shader.setFloat("steps",128.0f);
-        shader.setFloat("size", 0.3f);
-        shader.setBool("disk", true);
+        shader.setFloat("speed", speed);
+        shader.setInt("AA", AA);
+        shader.setFloat("steps",steps);
+        shader.setFloat("size", size);
+        shader.setBool("disk", showDisk);
 
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        {
+            ImGui::Begin("BLACK HOLE SETTINGS", nullptr);
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::SliderInt("Anti-Aliasing", &AA, 1, 8);
+            ImGui::SliderFloat("Black Hole Size", &size, 0.1f, 1.0f);
+            ImGui::SliderFloat("Black Hole Speed", &speed, 0.0f, 10.0f);
+            ImGui::SliderFloat("Simulation Steps", &steps, 2.0f, 64.0f);
+            ImGui::Checkbox("Show Disk", &showDisk);
+            ImGui::Checkbox("Fixed Camera", &fixedCamera);
+            ImGui::SliderFloat("Camera Rotation", &rotation, 0, SCR_HEIGHT);
+            ImGui::SliderFloat("Camera Distance", &distance, 0, SCR_WIDTH);
+            ImGui::End();
+        }
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
 
         glfwSwapBuffers(window);
 
